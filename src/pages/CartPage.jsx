f111f38fe9524b623294381f69a96b537ef0d086
Smith Livingston <smithlivingston2005@gmail.com>
2026-02-13 14:43:43 +0530

    Cart is updated

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -37,7 +37,7 @@ const CartPage = () => {
   const [formError, setFormError] = useState('');
   const fileInputRef = useRef(null);
 
-  const sparkzUPI = 'smithlivingston2005@okhdfcbank';
+  const sparkzUPI = 'kvbupiqr.105000000014533@kvb';
 
   // --- Logic: Pricing & Student Check ---
   const isKluStudent = useMemo(() => user?.email?.toLowerCase().endsWith('@klu.ac.in'), [user]);
